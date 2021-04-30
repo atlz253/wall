c1beffd00b9c6a0197cb7cc1d7cf87484437616b
Fedor <f.alexeeff2010@yandex.ru>
2021-04-30 17:46:51 +0300

    добавил и исправил новые цвета

--- src/ccolors.h
+++ src/ccolors.h
@@ -4,5 +4,7 @@ namespace ccolors
 {
     const std::string
         WHITE = "\033[0m",
-        RED = "\033[0;33m";
+        RED = "\033[0;31m",
+        GREEN = "\033[0;32m",
+        YELLOW = "\033[0;33m";
 }
