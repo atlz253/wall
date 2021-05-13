8da66f89eecdf3dd5d2a45ea78301431bd1867a2
Fedor <f.alexeeff2010@yandex.ru>
2021-05-13 22:19:29 +0300

    заполнение темных участков темнотой

--- src/world/background.cpp
+++ src/world/background.cpp
@@ -43,24 +43,33 @@ Background::Background(Renderer *renderer)
 {
     Entity *p;
 
-    printTrace("Background: инициализация неба");
+    printTrace("Terrain: инициализация заднего плана");
+    printTrace("Background: создаем небесную твердь");
     p = new Entity(renderer, "res/Magic-Cliffs-Environment/PNG/sky.png", 124, 608);
     _list.push_back(p);
     for (int i = 1; i < 11; i++)
         _list.push_back(new Entity(p, i * 124));
 
-    printTrace("Background: инициализация моря");
+    printTrace("Background: разливаем воду");
     p = new Entity(renderer, "res/Magic-Cliffs-Environment/PNG/sea.png", 124, 192, 0, 608);
     _list.push_back(p);
     for (int i = 1; i < 11; i++)
         _list.push_back(new Entity(p, i * 124, 608));
 
-    printTrace("Background: инициализация облаков");
+    printTrace("Background: сгущаем облака");
     p = new Clouds(renderer, "res/Magic-Cliffs-Environment/PNG/clouds.png", 1088, 472, 0, 136);
     _list.push_back(p);
     _list.push_back(new Clouds(p, -1088, 136));
     _list.push_back(new Clouds(p, 1088, 136));
 
-    printTrace("Background: инициализация далеких земель");
+    printTrace("Background: создаем далекие земели");
     _list.push_back(new Entity(renderer, "res/Magic-Cliffs-Environment/PNG/far-grounds.png", 1232, 220, 24, 388));
+
+    printTrace("Terrain: распыляем темноту");
+    p = new Entity(renderer, "res/Magic-Cliffs-Environment/PNG/tileset.png", 16 * 22, 16 * 6, 480, 560);
+    p->setTile(112, 272, 16, 16);
+    _list.push_back(p);
+    p = new Entity(p, 1120, 592);
+    p->setSize(16 * 10, 16 * 8);
+    _list.push_back(p);
 }
