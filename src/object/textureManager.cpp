32afe3e9a5dbafbf4667cfd67844555b6f2da595
Fedor <f.alexeeff2010@yandex.ru>
2021-05-15 19:27:00 +0300

    хранение текстур

---
+++ src/object/textureManager.cpp
@@ -0,0 +1,38 @@
+#include "print.hpp"
+#include "globals.hpp"
+#include "SDL_image.h"
+#include "renderer.hpp"
+#include "textureManager.hpp"
+
+TextureManager::TextureManager()
+{
+    printTrace("Инициализация менеджера текстур");
+}
+
+void TextureManager::loadTexture(std::string name, std::string path)
+{
+    printTrace("TextureManager: загрузка текстуры", path);
+    SDL_Texture *texture = IMG_LoadTexture(renderer->getRender(), path.c_str());
+
+    if (!texture)
+        printError("TextureManager: не удалось загрузить текстуру.", IMG_GetError());
+
+    _dict[name] = texture;
+}
+
+SDL_Texture *&TextureManager::key(std::string name)
+{
+    _iterator = _dict.find(name);
+
+    if (_iterator->first != name)
+        _dict[name] = nullptr; // на всякий случай создаем пару ключ:значение
+
+    return _dict[name];
+}
+
+TextureManager::~TextureManager()
+{
+    printTrace("TextureManager: удаление текстур из памяти");
+    for (_iterator = _dict.begin(); _iterator != _dict.end(); _iterator++)
+        SDL_DestroyTexture(_iterator->second);
+}
---
+++ src/object/textureManager.hpp
@@ -0,0 +1,33 @@
+#ifndef TEXTUREMANAGER
+#define TEXTUREMANAGER
+
+#include <map>
+#include <string>
+
+typedef struct SDL_Texture SDL_Texture;
+
+/*
+    Объект хранит в себе все загруженные текстуры в формате ключ:значение
+*/
+class TextureManager final
+{
+private:
+    std::map<std::string, SDL_Texture*> _dict; // контейнер текстур
+    std::map<std::string, SDL_Texture*> :: iterator _iterator; // итератор для работы с контейнером
+public:
+    TextureManager();
+
+    /*
+        Загрузка текстуры
+    */
+    void loadTexture(std::string name, std::string path);
+
+    /*
+        Получение объекта текстуры по ключу
+    */
+    SDL_Texture *&key(std::string name);
+
+    ~TextureManager();
+};
+
+#endif
